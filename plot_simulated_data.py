e102247366aa539011086642b219aa11e4f67d97
bthirion <bertrand.thirion@inria.fr>
2016-02-24 23:26:15 +0100

    Revision of the simulation module

--- plot_simulated_data.py
+++ plot_simulated_data.py
@@ -19,20 +19,24 @@ import nibabel
 
 ###############################################################################
 # Function to generate data
-def create_simulation_data(snr=0, n_samples=200, size=12, random_state=1):
+def create_simulation_data(snr=0, n_samples=200, size=12, random_state=1,
+                           modulation=False):
     generator = check_random_state(random_state)
     roi_size = 2
     smooth_X = 1
     ### Coefs
-    w = np.zeros((size, size, size))
-    w[0:roi_size, 0:roi_size, 0:roi_size] = -0.6
-    w[-roi_size:, -roi_size:, 0:roi_size] = 0.5
-    w[0:roi_size, -roi_size:, -roi_size:] = -0.6
-    w[-roi_size:, 0:roi_size:, -roi_size:] = 0.5
+    w = np.zeros((size, size, size, 5))
+    w[0:roi_size, 0:roi_size, 0:roi_size, 0] = -0.6
+    w[-roi_size:, -roi_size:, 0:roi_size, 1] = 0.5
+    w[0:roi_size, -roi_size:, -roi_size:, 2] = -0.6
+    w[-roi_size:, 0:roi_size:, -roi_size:, 3] = 0.5
     w[(size - roi_size) // 2:(size + roi_size) // 2,
       (size - roi_size) // 2:(size + roi_size) // 2,
-      (size - roi_size) // 2:(size + roi_size) // 2] = 0.5
-    w = w.ravel()
+      (size - roi_size) // 2:(size + roi_size) // 2, 4] = 0.5
+    if modulation:
+        w = np.array([w_. ravel() for w_ in w.T])
+    else:
+        w = w.sum(-1).ravel()[np.newaxis]
 
     ### Generate smooth background noise
     X_ = generator.randn(n_samples, size, size, size)
@@ -44,23 +48,25 @@ def create_simulation_data(snr=0, n_samples=200, size=12, random_state=1):
 
     noise = np.array(noise)
     ### Generate the signal y and X
-    y = generator.randn(n_samples)
-    X = np.dot(y[:, np.newaxis], w[np.newaxis])
-    X = X / (np.linalg.norm(w, 2) ** 2)
+    y_ = generator.randn(n_samples, 1)
+    if modulation:
+        y = y_ * np.random.rand(n_samples, 5)
+    else:
+        y = y_
+
+    X = np.dot(y, w)
 
     ## Generate the noise
     norm_noise = linalg.norm(X, 2) / np.exp(snr / 20.)
-    noise_coef = norm_noise / linalg.norm(noise, 2)
+    noise_coef = norm_noise / linalg.norm(noise[:, w.sum(0) != 0], 2)
     noise *= noise_coef
-    # snr = 20 * np.log(linalg.norm(X, 2) / linalg.norm(noise, 2))
-    # print("SNR: %.1f dB" % snr)
-
+ 
     ### Mixing of signal + noise and splitting into train/test
     ## Old noising version
     X += noise
     X -= X.mean(axis=-1)[:, np.newaxis]
     X /= X.std(axis=-1)[:, np.newaxis]
-    return X, y, snr, noise, w, size
+    return X, np.ravel(y), snr, noise, w, size
 
 
 def plot_slices(data, title=None):
